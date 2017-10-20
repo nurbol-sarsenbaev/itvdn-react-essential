d26a9784f602912096b6e38fd3899e6c583f0e15
Сарсенбаев Нурбол <kuma_official@mail.ru>
2017-10-20 22:23:51 +0600

    introduction to react

---
+++ 01-introduction-to-react/02-contacts-search/datas.js
@@ -0,0 +1,38 @@
+var CONTACTS = [
+    {
+        id: 1,
+        name: "Nurbol Sarsenbaev",
+        phoneNumber: "+77025634565",
+        image: "img/h-87.jpg"
+    },
+    {
+        id: 2,
+        name: "Aigerim Serdalieva",
+        phoneNumber: "+77752313256",
+        image: "img/Aiko.jpg"
+    },
+    {
+        id: 3,
+        name: "Aisha Jetkergenova",
+        phoneNumber: "+77075643212",
+        image: "img/aisha.jpeg"
+    },
+    {
+        id: 4,
+        name: "Aijan Izimova",
+        phoneNumber: "+77456523212",
+        image: "img/h-157.jpg"
+    },
+    {
+        id: 5,
+        name: "Janar Altayeva",
+        phoneNumber: "+77775641236",
+        image: "img/h-204.jpg"
+    },
+    {
+        id: 6,
+        name: "Bekzat Nurjanova",
+        phoneNumber: "+77774568912",
+        image: "img/ava.jpg"
+    }
+];
